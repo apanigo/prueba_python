93f5a5181577da26c2dd5f352c43790ce62dcc83
Ailen Panigo <ailenpanigo1@gmail.com>
2021-03-25 10:12:48 -0300

    creacion de archivo inicial

---
+++ run.py
@@ -0,0 +1,6 @@
+ # leer un número desde el teclado e imprimir si el número es o no par.
+num = int( input("Ingresá un número: "))
+if num % 2 == 0:
+    print("Es par")
+else:
+    print("No es par")
